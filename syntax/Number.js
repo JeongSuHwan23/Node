ce2a620b8887738a66e307ce30e8a2dce6e350d9
jeongsuhwan <short9666@gmail.com>
2024-03-06 16:37:29 +0900

    reset

--- syntax/Number.js
+++
@@ -1,4 +0,0 @@
-console.log(1+2);
-console.log(1-1);
-console.log(1*5);
-console.log(4/2);
--- syntax/Template.js
+++
@@ -1,6 +0,0 @@
-var name = 'ABC';
-var letter = `Dear ${name}
-We\'ve combined the power of the Following feed with the ${name} 
-For you feed so there’s one place to discover content on GitHub. 
-${name} There’s improved filtering so you can customize your feed exactly how you like it, and a shiny new visual design.`;
-console.log(letter);
